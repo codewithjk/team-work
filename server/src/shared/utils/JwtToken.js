50259ce40ef021987e70b2adf92fe3f382d4b3c6
jeevan <jeevankumar8943@gmail.com>
2025-01-06 10:16:27 +0530

    FIX : fixing jwt expitrIn to string , its not recegonised as string in server

--- server/src/shared/utils/JwtToken.js
+++ server/src/shared/utils/JwtToken.js
@@ -7,10 +7,10 @@ class Jwt {
     console.log("this is from twt tocken : ", user)
     const { _id, name, email } = user;
     const accessToken = jwt.sign({ userId: _id, name, email, role: "user" }, secret, {
-      expiresIn: process.env.ACCESSTOKEN_EXPIRES_IN,
+      expiresIn: String(process.env.ACCESSTOKEN_EXPIRES_IN),
     });
     const refreshToken = jwt.sign({ userId: _id, name, email, role: "user" }, secret, {
-      expiresIn: process.env.REFRESHTOKEN_EXPIRES_IN,
+      expiresIn: String(process.env.REFRESHTOKEN_EXPIRES_IN),
     });
     return { user, refreshToken, accessToken };
   }
